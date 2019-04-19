bfa8be5b7f5415b48577eca58e418371c8b6b4a7
Administrator <825978293@qq.com>
2019-04-19 17:32:59 +0800

    Revert "个人博客模板"
    This reverts commit 9897b370

--- myblog/views.py
+++
@@ -1,8 +0,0 @@
-from django.http import HttpResponse
-from django.shortcuts  import render
-
-def hello(request):
-    content = {}
-    content['hello'] = "helloworld"
-
-    return render(request,'hello.html',content)
